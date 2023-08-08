2f2db9ca555c704bf334a3b0376941649bbd517d
jadewang425 <jadewang425@gmail.com>
2023-08-08 17:11:53 -0400

    0808-EOD

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ const wrongGuessesEl = document.getElementById('wrongGuesses')
 const maxWrongGuessesEl = document.getElementById('maxWrongGuesses')
 const animals = ['cat', 'dog', 'mouse', 'goat', 'owl', 'elephant', 'eagle', 'rabbit']
 
+// test
+
 // set variables
 let board 
 let winner // true or false
@@ -24,7 +26,6 @@ function init () {
     winner = false
     revealCount = 0
     wrongGuesses = 0
-    // console.log('init wrongGuesses', wrongGuesses)
     activeCard = null // set to the first event click in each round to compare with the second click
     activeTimer = true // wait for the countdown to complete before click
     board = [
@@ -64,12 +65,7 @@ function startCountdown () {
     const timerId = setInterval(() => {
         count--
         if(count > 0) {
-            // console.log('interval running', count)
             messageEl.innerText = count
-            // disable click during countdown
-            // cardEls.forEach((card) => {
-
-            // })
         } else {
             clearInterval(timerId)
             messageEl.innerText = "Let's begin!"
@@ -90,53 +86,53 @@ function render() {
 function renderMessage() {
     // update wrong guesses number
     wrongGuessesEl.innerText = `${wrongGuesses}`
+    // winning message
     if (winner) {
         messageEl.innerText = `You found all of the animals!`
-        return
     }
+    //if reaches max wrong guesses number, show game over message
     if (wrongGuesses === Number(maxWrongGuessesEl.innerText)) {
     wrongGuessesMsgEl.style.color = 'red'
     messageEl.style.color = 'red'
     messageEl.innerText = 'Game Over'
-    return
     }
 }
 
 
 function handleChoice(evt) {
     // return if any timer is active
+    const animal = evt.target
     if (activeTimer || 
         // or clicked outside of the cards
-        evt.target.tagName !== 'DIV' ||
+        animal.tagName !== 'DIV' ||
         // or clicked on the same card
-        evt.target === activeCard ||
+        animal === activeCard ||
         // when max wrong guesses reached
         wrongGuesses === Number(maxWrongGuessesEl.innerText) ||
         // if win
         winner === true
-        ) {
-        return
-    } 
+        ) return
+
     // grab attribute of the card clicked
-    const animal = evt.target.getAttribute('data-animal')
-    evt.target.style.backgroundImage = `url(/imgs/${animal}.png)`
-    evt.target.style.backgroundColor = 'white'
+    const animalAtt = animal.getAttribute('data-animal')
+    animal.style.backgroundImage = `url(/imgs/${animalAtt}.png)`
+    animal.style.backgroundColor = 'white'
     // if this if the first card click, set as active card then return to proceed with second click
     if (!activeCard) {
-        activeCard = evt.target;
+        activeCard = animal;
         return
     }
     // if the second card doesn't match with the active card
     
-    if (animal !== activeCard.getAttribute('data-animal')) {
+    if (animalAtt !== activeCard.getAttribute('data-animal')) {
         // set to true so the player cannot click on more cards before the current cards are covered
     activeTimer = true;
     wrongGuesses += 1
 
     // setTimeout to reset cards after the second click not matched with the first to end current round
     setTimeout(() => {
-        evt.target.style.backgroundImage = null
-        evt.target.style.backgroundColor = 'lightgrey'
+        animal.style.backgroundImage = null
+        animal.style.backgroundColor = 'lightgrey'
         activeCard.style.backgroundImage = null
         activeCard.style.backgroundColor = 'lightgrey'
         // after clearing out the revealed cards
@@ -149,7 +145,7 @@ function handleChoice(evt) {
         activeCard = null
         revealCount += 2
 
-        messageEl.innerText = `You found the ${animal}s!`
+        messageEl.innerText = `You found the ${animalAtt}s!`
         messageEl.style.color = 'green'
     }
     // win if all animals are found
