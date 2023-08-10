a86e65091237ee97c065b13644f5c2e619232afd
jadewang425 <jadewang425@gmail.com>
2023-08-10 14:46:08 -0400

    re-organized comments and code

--- js/main.js
+++ js/main.js
@@ -1,53 +1,44 @@
 
 // define constance
-const boardEl = document.querySelector('#board')
 const cardEls = [...document.querySelectorAll('.card')]
 const cardFrontEls = [...document.querySelectorAll('.cardFront')]
-const cardBackEls = [...document.querySelectorAll('.cardBack')]
 const resetGameBtn = document.querySelector('#resetGame')
 const messageEl = document.querySelector('h2');
 const wrongGuessesMsgEl = document.getElementById('wrongGuessesMsg')
 const wrongGuessesEl = document.getElementById('wrongGuesses')
 const maxWrongGuessesEl = document.getElementById('maxWrongGuesses')
 const animals = ['cat', 'dog', 'mouse', 'goat', 'owl', 'elephant', 'eagle', 'rabbit']
-//console.log(boardEl)
 
 // set variables
-let board 
 let winner // true or false
-let revealCount // number os cards revealed to check win
-let wrongGuesses // count of the wrong guesses
+let revealCount // number of cards revealed to check win
+let wrongGuesses // count of wrong guesses
 let activeCard //lookup for the second card clicked
 let activeTimer // if any timer is active 
 
 init()
 // initialize the game
+
 function init () {
-    // 
     const totalAnimals = [...animals, ...animals]
     winner = false
     revealCount = 0
     wrongGuesses = 0
     activeCard = null // set to the first event click in each round to compare with the second click
     activeTimer = true // wait for the countdown to complete before click
-    // board = [
-    //     0, 0, 0, 0,
-    //     0, 0, 0, 0, 
-    //     0, 0, 0, 0, 
-    //     0, 0, 0, 0 
-    // ]
-    // reset text color & board
+
+    // reset text color & cards
     messageEl.classList.remove('wrongGuessesMax')
     wrongGuessesMsgEl.classList.remove('wrongGuessesMax')
     messageEl.classList.remove('foundAll')
     cardEls.forEach(card => card.classList.remove('flipped'))
     
-    // assigning cards with random animal options in the initial board
+    // assigning animals to cards randomly
     shuffleCards(totalAnimals)
     // countdown before the cards are hidden
     startCountdown()
     // render message
-    render()
+    renderMessage()
 }
 // shuffle and assign cards
 function shuffleCards(arr) {
@@ -55,7 +46,7 @@ function shuffleCards(arr) {
         // pick a random index in the totalAnimals array to assign to a card
         const randomIdx = Math.floor(Math.random()*arr.length)
         const animal = arr[randomIdx]
-
+        // create an attribute to assign animals
         cardEls[i].setAttribute('data-animal', animal)
         cardFrontEls[i].style.backgroundImage = `url(./imgs/${animal}.png)`
 
@@ -76,7 +67,6 @@ function startCountdown () {
             clearInterval(timerId)
             messageEl.innerText = "Let's begin!"
             cardEls.forEach((e) => {
-
             e.classList.add('flipped')
             activeTimer = false
             })
@@ -84,20 +74,15 @@ function startCountdown () {
     }, 1000)
 }
 
-function render() {
-//     renderBoard()
-    renderMessage()
-}
-
 function renderMessage() {
-    // update wrong guesses number
+    // update the count of wrong guesses
     wrongGuessesEl.innerText = `${wrongGuesses}`
     // winning message
     if (winner) {
         messageEl.classList.add('foundAll')
         messageEl.innerText = `You found all of the animals!`
     }
-    //if reaches max wrong guesses number, show game over message
+    //if reaches max wrong guesses count, show game over message
     if (wrongGuesses === Number(maxWrongGuessesEl.innerText)) {
     wrongGuessesMsgEl.classList.add('wrongGuessesMax')
     messageEl.classList.add('wrongGuessesMax')
@@ -107,35 +92,28 @@ function renderMessage() {
 
 
 function handleChoice(evt) {
-    // return if any timer is active
     const currentCard = evt.target
     const currentCardP = currentCard.parentElement
-    if (activeTimer || 
-        // or clicked outside of the cards
-        currentCard.tagName !== 'DIV' ||
-        // or clicked on the same card
-        currentCard === activeCard ||
+    console.log(currentCard)
+    // return if any timer is active
+    if (activeTimer ||
         // when max wrong guesses reached
         wrongGuesses === Number(maxWrongGuessesEl.innerText) ||
         // if win
         winner === true
-        ) return
+        ) return        
 
-        //const choiceIdx = cardBackEls.findIndex(evt.target.innerHTML)
-        console.log('handleChoice, evt parent el: ', currentCardP)
-        
-
-    // grab attribute of the current and active card
-    const currentCardAtt = currentCardP.getAttribute('data-animal')
-    
-    console.log(currentCardAtt)
+    // reveal the card
     currentCardP.classList.remove('flipped');
+    // get the animal assigned to current card
+    const currentCardAtt = currentCardP.getAttribute('data-animal')
 
-    // if this if the first card click, set as active card then return to proceed with second click
+    // if this is the first card, set as active card then return
     if (!activeCard) {
         activeCard = currentCard;
         return
-    }
+    // if click on the same card, return immediately
+    } else if (currentCard.id === activeCard.id) return
     // if the second card doesn't match with the active card
     if (currentCardAtt !== activeCard.parentElement.getAttribute('data-animal')) {
         // set to true so the player cannot click on more cards before the current cards are covered
@@ -155,20 +133,19 @@ function handleChoice(evt) {
         activeTimer = false
         activeCard = null
         revealCount += 2
-
+        
         messageEl.innerText = `You found the ${currentCardAtt}s!`
     }
     // win if all animals are found
     if (revealCount === (animals.length)*2) {
         winner = true
     }
-    render()
+    renderMessage()
 }
 
 
 // eventListeners
 //card clicked
-boardEl.addEventListener('click', handleChoice)
-
+cardEls.forEach(card => card.addEventListener('click', handleChoice))
 //reset game button click
 resetGameBtn.addEventListener('click', init)
