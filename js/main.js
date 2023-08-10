5df0e52fcee453f0ee074ed027ee12b2fb711223
jadewang425 <jadewang425@gmail.com>
2023-08-10 13:51:51 -0400

    add flip card code

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 
 // define constance
 const boardEl = document.querySelector('#board')
-const messageEl = document.querySelector('h2');
 const cardEls = [...document.querySelectorAll('.card')]
 const cardFrontEls = [...document.querySelectorAll('.cardFront')]
+const cardBackEls = [...document.querySelectorAll('.cardBack')]
 const resetGameBtn = document.querySelector('#resetGame')
+const messageEl = document.querySelector('h2');
 const wrongGuessesMsgEl = document.getElementById('wrongGuessesMsg')
 const wrongGuessesEl = document.getElementById('wrongGuesses')
 const maxWrongGuessesEl = document.getElementById('maxWrongGuesses')
 const animals = ['cat', 'dog', 'mouse', 'goat', 'owl', 'elephant', 'eagle', 'rabbit']
-// test
-// console.log(cardEls)
+//console.log(boardEl)
+
 // set variables
 let board 
 let winner // true or false
@@ -29,12 +30,12 @@ function init () {
     wrongGuesses = 0
     activeCard = null // set to the first event click in each round to compare with the second click
     activeTimer = true // wait for the countdown to complete before click
-    board = [
-        0, 0, 0, 0,
-        0, 0, 0, 0, 
-        0, 0, 0, 0, 
-        0, 0, 0, 0 
-    ]
+    // board = [
+    //     0, 0, 0, 0,
+    //     0, 0, 0, 0, 
+    //     0, 0, 0, 0, 
+    //     0, 0, 0, 0 
+    // ]
     // reset text color & board
     messageEl.classList.remove('wrongGuessesMax')
     wrongGuessesMsgEl.classList.remove('wrongGuessesMax')
@@ -42,18 +43,6 @@ function init () {
     
     // assigning cards with random animal options in the initial board
     shuffleCards(totalAnimals)
-    // for (i = 0; i < board.length; i++) {
-    //     const cardEl = cardEls[i]
-    //     // pick a random index in the totalAnimals array to assign to a card
-    //     const randomIdx = Math.floor(Math.random()*totalAnimals.length)
-    //     const animal = totalAnimals[randomIdx]
-    //     cardEl.setAttribute('data-animal', animal)
-    //     cardEl.style.backgroundImage = `url(./imgs/${animal}.png)`
-    //     // cardEl.style.backgroundColor = 'white'
-        
-    //     //take out the random index once picked
-    //     totalAnimals.splice(randomIdx, 1)
-    // }
     // countdown before the cards are hidden
     startCountdown()
     // render message
@@ -65,9 +54,9 @@ function shuffleCards(arr) {
         // pick a random index in the totalAnimals array to assign to a card
         const randomIdx = Math.floor(Math.random()*arr.length)
         const animal = arr[randomIdx]
-        console.log(cardEls.length)
+
         cardEls[i].setAttribute('data-animal', animal)
-        //how to get image to assign to the card front?
+        cardFrontEls[i].style.backgroundImage = `url(./imgs/${animal}.png)`
 
         //take out the random index once picked
         arr.splice(randomIdx, 1)
@@ -86,8 +75,7 @@ function startCountdown () {
             clearInterval(timerId)
             messageEl.innerText = "Let's begin!"
             cardEls.forEach((e) => {
-            // e.style.backgroundImage = null
-            // e.style.backgroundColor = 'lightgrey'
+
             e.classList.add('flipped')
             activeTimer = false
             })
@@ -119,52 +107,53 @@ function renderMessage() {
 
 function handleChoice(evt) {
     // return if any timer is active
-    const animal = evt.target
-    console.log(animal)
+    const currentCard = evt.target
+    const currentCardP = currentCard.parentElement
     if (activeTimer || 
         // or clicked outside of the cards
-        animal.tagName !== 'DIV' ||
+        currentCard.tagName !== 'DIV' ||
         // or clicked on the same card
-        animal === activeCard ||
+        currentCard === activeCard ||
         // when max wrong guesses reached
         wrongGuesses === Number(maxWrongGuessesEl.innerText) ||
         // if win
         winner === true
         ) return
 
-    // grab attribute of the card clicked
-    const animalAtt = animal.getAttribute('data-animal')
-    animal.classList.add('flipped');
-    // animal.style.backgroundImage = `url(./imgs/${animalAtt}.png)` // update image url with .
-    // animal.style.backgroundColor = 'white'
+        //const choiceIdx = cardBackEls.findIndex(evt.target.innerHTML)
+        console.log('handleChoice, evt parent el: ', currentCardP)
+        
+
+    // grab attribute of the current and active card
+    const currentCardAtt = currentCardP.getAttribute('data-animal')
+    const activeCardAtt = activeCard.parentElement.getAttribute('data-animal')
+    console.log(currentCardAtt)
+    currentCardP.classList.remove('flipped');
+
     // if this if the first card click, set as active card then return to proceed with second click
     if (!activeCard) {
-        activeCard = animal;
+        activeCard = currentCard;
         return
     }
     // if the second card doesn't match with the active card
-    if (animalAtt !== activeCard.getAttribute('data-animal')) {
+    if (currentCardAtt !== activeCardAtt) {
         // set to true so the player cannot click on more cards before the current cards are covered
-    activeTimer = true;
-    wrongGuesses += 1
+        activeTimer = true;
+        wrongGuesses += 1
 
-    // setTimeout to reset cards after the second click not matched with the first to end current round
-    setTimeout(() => {
-        animal.style.backgroundImage = null
-        animal.style.backgroundColor = 'lightgrey'
-        activeCard.style.backgroundImage = null
-        activeCard.style.backgroundColor = 'lightgrey'
-        // after clearing out the revealed cards
-        activeTimer = false
-        activeCard = null
+        // setTimeout to reset cards after the second click not matched with the first to end current round
+        setTimeout(() => {
+            currentCardP.classList.add('flipped')
+            activeCard.parentElement.classList.add('flipped')
+            // after clearing out the revealed cards
+            activeTimer = false
+            activeCard = null
     }, 1000)
     } else {
         // if the second card match with the first card
         activeTimer = false
         activeCard = null
         revealCount += 2
-
-        messageEl.innerText = `You found the ${animalAtt}s!`
     }
     // win if all animals are found
     if (revealCount === (animals.length)*2) {
